d0424333d16c60fce6824e07b05fa09de55fdf7a
Gabriel Sargeant <gabe.sargeant@gmail.com>
2020-10-20 00:01:40 +1100

    wip

--- co-tree.go
+++ co-tree.go
@@ -1,14 +1,16 @@
 package main
 
 //AsgsRegionNode One region in all the regions, Let the nodes begin!
+// This is a doubly linked node. ie it points up and down. 
+// I reserve the right to decide if I'm going to change this.
 type AsgsRegionNode struct{
 	RegionID string
 	RegionName string
 	ParentRegionID *AsgsRegionNode
-	ChildRegions []string
+	ChildRegions []*AsgsRegionNode
 }
 
 func main(){
-	
+
 
 }
