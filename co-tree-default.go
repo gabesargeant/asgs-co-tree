a1ba24ccb121d203b79196ae432175c22dfafddd
Gabriel Sargeant <gabe.sargeant@gmail.com>
2021-03-07 22:11:46 +1100

    rethinking!

--- co-tree-default.go
+++ co-tree-default.go
@@ -12,10 +12,10 @@ import (
 //Region region struct
 type Region struct {
 	RegionID       string `json:"RegionID,omitempty"`
-	parentRegionID string
+	parentRegionID map[string]string
 	RegionName     string            `json:"RegionName,omitempty"`
 	LevelType      string            `json:"LevelType,omitempty"`
-	ChildRegions   map[string]Region `json:"ChildRegions,omitempty"`
+	ChildRegions   map[string]map[string]Region `json:"ChildRegions,omitempty"`
 }
 
 //LevelName, Level Code
@@ -25,171 +25,176 @@ var gccsaRegionMap = make(map[string]Region)
 var sscRegionMap = make(map[string]Region)
 var poaRegionMap = make(map[string]Region)
 
-var regionMap = map[string]map[string]Region{
-	"asgs":  asgsRegionMap,
-	"lga":   lgaRegionMap,
-	"gccsa": gccsaRegionMap,
-	"ssc":   sscRegionMap,
-	"poa":   poaRegionMap,
-}
+var regionMap = map[string]map[string]Region{};
 
-//regionsetsmap[parentcode][childcode][child]
-var asgsRegionSetsMap = make(map[string]map[string]Region)
-var lgaRegionSetsMap = make(map[string]map[string]Region)
-var gccsaRegionSetsMap = make(map[string]map[string]Region)
-var sscRegionSetsMap = make(map[string]map[string]Region)
-var poaRegionSetsMap = make(map[string]map[string]Region)
-
-var regionSetsMaps = map[string]map[string]map[string]Region{
-	"asgs":  asgsRegionSetsMap,
-	"lga":   lgaRegionSetsMap,
-	"gccsa": gccsaRegionSetsMap,
-	"ssc":   sscRegionSetsMap,
-	"poa":   poaRegionSetsMap,
-}
+var regionSetsMap = make(map[string]map[string]Region)
 
 var totalRegions float64 = 0
 
-var asgsLevelSeq = []string{
-	"AUS",
-	"STE",
-	"SA4",
-	"SA3",
-	"SA2",
-	"SA1",
-	"MB",
+var asgsLevelSeq = map[string][]string{
+	"AUS":{"STE"},
+	"STE":{"SA4", "SOS", "GCCSA", "IREG", "SUA", "RA", "POA", "CED", "SED", "SSC", "ADD", "NRMR", "LGA"},
+	"SA4":{"SA3"},
+	"SA3":{"SA2"},
+	"SA2":{"SA1"},
+	"SA1":{"MB"},
+	"MB":{},
+	"SOS":{"SOSR"},
+	"SOSR":{"UCL"},
+	"UCL":{"SA1"},
+	"GCCSA":{"SA4"},
+	"SUA": {"SA3"},
+	"RA": {"SA1"},
+	"IRGE":{"IARE"},
+	"IARE":{"ILOC"},
+	"ILOC":{"SA1"},
+	"POA":{}, 
+	"CED":{}, 
+	"SED":{}, 
+	"SSC":{}, 
+	"ADD":{}, 
+	"NRMR":{}, 
+	"LGA":{},
 }
-
-var gccsaLevelSeq = []string{
-	"AUS",
-	"STE",
-	"GCCSA",
-}
-
-var lgaLevelSeq = []string{
-	"AUS",
-	"STE",
-	"LGA",
-}
-
-var sscLevelSeq = []string{
-	"AUS",
-	"STE",
-	"SSC",
-	//"MB",
-}
-
-var poaLevelSeq = []string{
-	"AUS",
-	"STE",
-	"POA",
-	//"MB",
-}
-
-var levelSequences = map[string][]string{
-	"asgs":  asgsLevelSeq,
-	"lga":   lgaLevelSeq,
-	"ssc":   sscLevelSeq,
-	"poa":   poaLevelSeq,
-	"gccsa": gccsaLevelSeq,
-}
-
-var levelCodeMap = map[string]string{
-
-	"MB":    "MB_CODE_2016",
-	"SA1":   "SA1_MAINCODE_2016",
-	"SA2":   "SA2_MAINCODE_2016",
-	"SA3":   "SA3_CODE_2016",
-	"SA4":   "SA4_CODE_2016",
-	"STE":   "STATE_CODE_2016",
-	"AUS":   "AUS_CODE_2016",
-	"LGA":   "LGA_CODE_2020",
-	"POA":   "POA_CODE_2016",
-	"SSC":   "SSC_CODE_2016",
-	"GCCSA": "GCCSA_CODE_2016",
-}
-
-var levelNameMap = map[string]string{
-
-	"MB":    "MB_CATEGORY_NAME_2016",
-	"SA1":   "SA1_NAME_2016",
-	"SA2":   "SA2_NAME_2016",
-	"SA3":   "SA3_NAME_2016",
-	"SA4":   "SA4_NAME_2016",
-	"STE":   "STATE_NAME_2016",
-	"AUS":   "AUS_NAME_2016",
-	"LGA":   "LGA_NAME_2020",
-	"POA":   "POA_NAME_2016",
-	"SSC":   "SSC_NAME_2016",
-	"GCCSA": "GCCSA_NAME_2016",
-}
-
-var asgsParentLevel = map[string]string{
-	"MB":  "SA1",
-	"SA1": "SA2",
-	"SA2": "SA3",
-	"SA3": "SA4",
-	"SA4": "STE",
-	"STE": "AUS",
-	"AUS": "",
-}
-
-var lgaParentLevel = map[string]string{
-	"LGA": "STE",
-	"STE": "AUS",
-	"AUS": "",
-}
-
-var gccsaParentLevel = map[string]string{
-	"GCCSA": "STE",
-	"STE":   "AUS",
-	"AUS":   "",
-}
-
-var sscParentLevel = map[string]string{
-	"MB":  "SSC",
-	"SSC": "STE",
-	"STE": "AUS",
-	"AUS": "",
-}
-var poaParentLevel = map[string]string{
-	"MB":  "POA",
-	"POA": "STE",
-	"STE": "AUS",
-	"AUS": "",
+var asgsParentSeq = map[string][]string{
+	"AUS":{},
+	"STE":{"AUS"},
+	"SA4":{"STE"},
+	"SA3":{"SA3"},
+	"SA2":{"SA3", "SUA"},
+	"SA1":{"SA2","RA", "UCL", "ILOC"},
+	"MB":{"SA1"},
+	"SOS":{"STE"},
+	"SOSR":{"SOS"},
+	"UCL":{"SOSR"},
+	"GCCSA":{"STE"},
+	"SUA": {"STE"},
+	"RA": {"STE"},
+	"IREG":{"STE"},
+	"IARE":{"IREG"},
+	"ILOC":{"IARE"},
+	"POA":{"STE"}, 
+	"CED":{"STE"}, 
+	"SED":{"STE"}, 
+	"SSC":{"STE"}, 
+	"ADD":{"STE"}, 
+	"NRMR":{"STE"}, 
+	"LGA":{"STE"},
 }
 
 var parentLevel = map[string]map[string]string{
-	"asgs":  asgsParentLevel,
-	"lga":   lgaParentLevel,
-	"gccsa": gccsaParentLevel,
-	"ssc":   sscParentLevel,
-	"poa":   poaParentLevel,
+
+}
+
+var levelCodeMap = map[string]string{
+"MB":"MB_CODE_2016",
+"SA1":"SA1_MAINCODE_2016",
+"SA2":"SA2_MAINCODE_2016",
+"SA3":"SA3_CODE_2016",
+"SA4":"SA4_CODE_2016",
+"GCCSA":"GCCSA_CODE_2016",
+"STE":"STATE_CODE_2016",
+"AUS":"AUS_CODE_2016",
+"DZN":"DZN_CODE_2016",
+"LGA":"LGA_CODE_2016",
+"POA":"POA_CODE_2016",
+"ADD":"ADD_CODE_2016",
+"NRMR":"NRMR_CODE_2016",
+"SSC":"SSC_CODE_2016",
+"TR":"TR_CODE_2016",
+"RA":"RA_CODE_2016",
+"ILOC":"ILOC_CODE_2016",
+"IARE":"IARE_CODE_2016",
+"IREG":"IREG_CODE_2016",
+"UCL":"UCL_CODE_2016",
+"SOSR":"SOSR_CODE_2016",
+"SOS":"SOS_CODE_2016",
+"SUA":"SUA_CODE_2016",
+"SED":"SED_CODE_2016",
+"CED":"CED_CODE_2016",
+}
+
+var levelNameMap = map[string]string{
+"MB":"MB_CATEGORY_NAME_2016",
+"SA1":"SA1_NAME_2016",
+"SA2":"SA2_NAME_2016",
+"SA3":"SA3_NAME_2016",
+"SA4":"SA4_NAME_2016",
+"GCCSA":"GCCSA_NAME_2016",
+"STE":"STATE_NAME_2016",
+"AUS":"AUS_NAME_2016",
+"DZN":"DZN_NAME_2016",
+"LGA":"LGA_NAME_2016",
+"POA":"POA_NAME_2016",
+"ADD":"ADD_NAME_2016",
+"NRMR":"NRMR_NAME_2016",
+"SSC":"SSC_NAME_2016",
+"TR":"TR_NAME_2016",
+"RA":"RA_NAME_2016",
+"ILOC":"ILOC_NAME_2016",
+"IARE":"IARE_NAME_2016",
+"IREG":"IREG_NAME_2016",
+"UCL":"UCL_NAME_2016",
+"SOSR":"SOSR_NAME_2016",
+"SOS":"SOS_NAME_2016",
+"SUA":"SUA_NAME_2016",
+"SED":"SED_NAME_2016",
+"CED":"CED_NAME_2016",
 }
 
 var asgsRegionArray = []string{
-	"MB_CODE_2016",
-	"MB_CATEGORY_NAME_2016",
-	"SA1_MAINCODE_2016",
-	"SA1_NAME_2016",
-	"SA2_MAINCODE_2016",
-	"SA2_NAME_2016",
-	"SA3_CODE_2016",
-	"SA3_NAME_2016",
-	"SA4_CODE_2016",
-	"SA4_NAME_2016",
-	"STATE_CODE_2016",
-	"STATE_NAME_2016",
-	"AUS_CODE_2016",
-	"AUS_NAME_2016",
-	"LGA_CODE_2020",
-	"LGA_NAME_2020",
-	"POA_CODE_2016",
-	"POA_NAME_2016",
-	"SSC_CODE_2016",
-	"SSC_NAME_2016",
-	"GCCSA_CODE_2016",
-	"GCCSA_NAME_2016",
+"MB_CODE_2016",
+"MB_CATEGORY_NAME_2016",
+"SA1_MAINCODE_2016",
+"SA1_NAME_2016",
+"SA2_MAINCODE_2016",
+"SA2_NAME_2016",
+"SA3_CODE_2016",
+"SA3_NAME_2016",
+"SA4_CODE_2016",
+"SA4_NAME_2016",
+"GCCSA_CODE_2016",
+"GCCSA_NAME_2016",
+"STATE_CODE_2016",
+"STATE_NAME_2016",
+"AUS_CODE_2016",
+"AUS_NAME_2016",
+"DZN_CODE_2016",
+"DZN_NAME_2016",
+"LGA_NAME_2015",
+"LGA_CODE_2015",
+"LGA_CODE_2016",
+"LGA_NAME_2016",
+"POA_CODE_2016",
+"POA_NAME_2016",
+"ADD_CODE_2016",
+"ADD_NAME_2016",
+"NRMR_CODE_2016",
+"NRMR_NAME_2016",
+"SSC_CODE_2016",
+"SSC_NAME_2016",
+"TR_CODE_2016",
+"TR_NAME_2016",
+"RA_CODE_2016",
+"RA_NAME_2016",
+"ILOC_CODE_2016",
+"ILOC_NAME_2016",
+"IARE_CODE_2016",
+"IARE_NAME_2016",
+"IREG_CODE_2016",
+"IREG_NAME_2016",
+"UCL_CODE_2016",
+"UCL_NAME_2016",
+"SOSR_CODE_2016",
+"SOSR_NAME_2016",
+"SOS_CODE_2016",
+"SOS_NAME_2016",
+"SUA_CODE_2016",
+"SUA_NAME_2016",
+"SED_CODE_2016",
+"SED_NAME_2016",
+"CED_CODE_2016",
+"CED_NAME_2016",
 }
 
 //getHeaderMap -
@@ -215,9 +220,9 @@ func getHeaderMap(firstLine []string) map[string]int {
 	return m
 }
 
-func buildNodeSet(headerMap map[string]int, row []string, level string, levelSequence []string) {
+func buildNodeSet(headerMap map[string]int, row []string, parentSeq map[string][]string) {
 
-	for _, currentLevel := range levelSequence {
+	for currentLevel, parentRegions := range parentSeq {
 
 		levelCode := levelCodeMap[currentLevel]
 		levelName := levelNameMap[currentLevel]
@@ -226,8 +231,8 @@ func buildNodeSet(headerMap map[string]int, row []string, level string, levelSeq
 		iLevelCode := row[headerMap[levelCode]]
 		iLevelName := row[headerMap[levelName]]
 
-		region := regionMap[level][iLevelCode]
-
+		region := regionMap[currentLevel][iLevelCode]
+		
 		if region.RegionID == "" {
 
 			//region.LevelIDName = levelCode
@@ -235,26 +240,19 @@ func buildNodeSet(headerMap map[string]int, row []string, level string, levelSeq
 
 			region.RegionName = iLevelName
 			region.RegionID = iLevelCode
-			region.ChildRegions = make(map[string]Region)
+			region.ChildRegions = make(map[string]map[string]Region)
 
 		}
 
 		//Add parent relationship
-		parent := parentLevel[level][currentLevel]
-		//fmt.Printf("Parent %d ", parent)
-		if parent == "" {
+		//parent := parentLevel[level][currentLevel]
 
-			regionMap[level][iLevelCode] = region
-			continue
+		for _, parentRegion := range parentRegions{
+			parentRegionID := row[headerMap[levelCodeMap[parentRegion]]]
+			region.parentRegionID[parentRegion] = parentRegionID
 		}
-		parentRegionID := row[headerMap[levelCodeMap[parent]]]
 
-		//fmt.Printf("region: %s \n", region.RegionID)
-		//fmt.Printf("parent: %s \n", parentRegionID)
-
-		region.parentRegionID = parentRegionID
-
-		regionMap[level][iLevelCode] = region
+		regionMap[currentLevel][iLevelCode] = region
 
 	}
 
@@ -269,11 +267,9 @@ func buildNodes(headerMap map[string]int, r *csv.Reader) {
 			//for the last record, write the buffer without the commaNewLine
 			break
 		}
-
-		for level, levelSequence := range levelSequences {
-
-			buildNodeSet(headerMap, row, level, levelSequence)
-		}
+		
+		buildNodeSet(headerMap, row, asgsParentSeq)
+		
 
 	}
 }
